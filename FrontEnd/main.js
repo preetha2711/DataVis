dab71ea2684156a591d755af5acf32ea80f6f148
Vineet Reddy <hotshot2797@gmail.com>
2018-02-23 18:18:35 +0530

    Added functionality to /main.js

--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -11,6 +11,8 @@ var chapters = {
     'rank-2'
 }
 
+var map;
+
 window.onload = function() {
     map = new mapboxgl.Map({
         container: 'map',
@@ -20,4 +22,35 @@ window.onload = function() {
         bearing: 0,
         pitch: 0,
     });
+}
+
+// On every scroll event, check which element is on screen
+window.onscroll = function() {
+    var chapterNames = Object.keys(chapters);
+    console.log(chapterNames);
+    for (var i = 0; i < chapterNames.length; i++) {
+        var chapterName = chapterNames[i];
+        if (isElementOnScreen(chapterName)) {
+            setActiveChapter(chapterName);
+            break;
+        }
+    }
+};
+
+var activeChapterName = 'rank-1';
+function setActiveChapter(chapterName) {
+    if (chapterName === activeChapterName) return;
+
+    map.flyTo(chapters[chapterName]);
+
+    document.getElementById(chapterName).setAttribute('class', 'active');
+    document.getElementById(activeChapterName).setAttribute('class', '');
+
+    activeChapterName = chapterName;
+}
+
+function isElementOnScreen(id) {
+    var element = document.getElementById(id);
+    var bounds = element.getBoundingClientRect();
+    return bounds.top < window.innerHeight && bounds.bottom > 0;
 }
